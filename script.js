da15443349ebe47b87e483f23894b92601604aee
elysian-place <153707210+leonardosite@users.noreply.github.com>
2024-03-18 10:58:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ let redes = document.querySelector('#dados-unico');
 redes.addEventListener('mouseover', function(){
  redes.classList.add('fundo-contatos')
 })
+
+const cliquemenu = document.querySelector('#menu');
+const fundo = document.querySelector('.fundo-menumob')
+const fechar = document.querySelector('#fechar-menumob')
+
+cliquemenu.addEventListener('click', ()=>{
+ fundo.classList.add('ativar-menumob')
+})
+fechar.addEventListener('click', ()=>{
+    fundo.classList.remove('ativar-menumob')
+   })
